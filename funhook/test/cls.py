2d5c6725f1d9704d3076877b3710ee6aac735a8c
mission.liao <missionaryliao@gmail.com>
2013-07-26 17:56:07 +0800

    refine style
    moving test-case specific thing into test-case.

--- funhook/test/cls.py
+++ funhook/test/cls.py
@@ -8,8 +8,9 @@ Created on Jul 23, 2013
 import unittest
 import funhook
 
+
 """
-    Hooks for test
+    Global Hook for testing
 """
 
 class h_test(funhook.Hook):
@@ -21,90 +22,6 @@ class h_test(funhook.Hook):
         return (n+1, )
 
 
-class h_accept_self(funhook.Hook):
-    def __init__(self):
-        super(h_accept_self, self).__init__()
-        self.accept_kwargs = False
-        self.accept_self = True
-        self.accept_pos_args = True
-
-    def before(self, inst, n):
-        inst.chk = False
-        return (n+2, )
-
-
-class clsh_set_prop(funhook.ClsHook):
-    def __init__(self):
-        super(clsh_set_prop, self).__init__()
-        self.accept_kwargs = False
-        self.accept_self = True
-        self.accept_pos_args = True
-
-    def before(self, klass):
-        return klass
-
-
-class clsh_with_after(funhook.ClsHook):
-    def __init__(self):
-        super(clsh_with_after, self).__init__()
-
-    def before(self, klass):
-        return klass
-
-    def after(self, klass):
-        return klass
-
-
-"""
-    Targets(functions, classes) for testing
-"""
-
-@funhook.attach_([h_test()])
-def test(n):
-    return n+1
-
-
-class cls_(object):
-    def __init__(self):
-        self.chk = True
-
-    @funhook.attach_([h_test()])
-    def sample_func(self, n):
-        return n + 1
-    
-    @funhook.attach_([h_accept_self()])
-    def sample_func_accept_self(self, n):
-        return n + 3
-
-
-@funhook.setup_([])
-class cls_no_side_effect(object):
-    def __init__(self):
-        self.to_dump = self.__class__.__name__
-        
-    def __call__(self):
-        return self.to_dump
-
-
-@funhook.setup_([])
-class cls_inhert_parent(object):
-    def __init__(self):
-        pass
-
-    @funhook.attach_([h_test()]) 
-    def func(self, n):
-        return n + 1
-
-
-@funhook.setup_([])
-class cls_inhert_child(cls_inhert_parent):
-    def __init__(self):
-        pass
-
-    def func(self, n):
-        return n + 100
-
-
 """
     Test Cases
 """
@@ -114,16 +31,39 @@ class TestClass(unittest.TestCase):
     Test-cases for applying hooks for member-functions
     in a class.
     """
+
     def test_basic_usage(self):
+        """
+        Basic Usage
+        """
+
+        class h_accept_self(funhook.Hook):
+            def __init__(self):
+                super(h_accept_self, self).__init__()
+                self.accept_kwargs = False
+                self.accept_self = True
+                self.accept_pos_args = True
+
+            def before(self, inst, n):
+                inst.chk = False
+                return (n+2, )
+    
+        class cls_(object):
+            def __init__(self):
+                self.chk = True
+
+            @funhook.attach_([h_test()])
+            def sample_func(self, n):
+                return n + 1
+    
+            @funhook.attach_([h_accept_self()])
+            def sample_func_accept_self(self, n):
+                return n + 3
+                
         sc = cls_()
         self.assertEqual(sc.sample_func(1), 3)
         self.assertEqual(sc.sample_func(5), 7) # run twice to make sure not state is kept
         
-    def test_accept_self(self):
-        """
-        make sure 'accept_self' option works
-        """
-        sc = cls_()
         self.assertEqual(sc.sample_func_accept_self(1), 6)
         self.assertEqual(sc.chk, False)
         
@@ -132,6 +72,11 @@ class TestClass(unittest.TestCase):
         make sure Hook can work on 'method' and 'function'
         as long as its signature is matched.
         """
+
+        @funhook.attach_([h_test()])
+        def test(n):
+            return n+1
+
         self.assertEqual(test(1), 3)
         
     def test_no_side_effect(self):
@@ -139,6 +84,15 @@ class TestClass(unittest.TestCase):
         make sure the class object returned by funhook.setup_
         works properly
         """
+
+        @funhook.setup_([])
+        class cls_no_side_effect(object):
+            def __init__(self):
+                self.to_dump = self.__class__.__name__
+        
+            def __call__(self):
+                return self.to_dump
+
         c = cls_no_side_effect()
         # TODO: test more aspect
         self.assertEqual(c(), c.__class__.__name__)
@@ -148,6 +102,18 @@ class TestClass(unittest.TestCase):
         make sure exception is raised when setting
         property of ClsHook
         """
+
+        class clsh_set_prop(funhook.ClsHook):
+            def __init__(self):
+                super(clsh_set_prop, self).__init__()
+                self.accept_kwargs = False
+                self.accept_self = True
+                self.accept_pos_args = True
+                self.accept_ret = True
+
+            def before(self, klass):
+                return klass
+
         self.assertRaises(Exception, clsh_set_prop)
         
     def test_clsh_with_after(self):
@@ -155,6 +121,17 @@ class TestClass(unittest.TestCase):
         make sure exception is raised when 'after'
         callback is defined
         """
+
+        class clsh_with_after(funhook.ClsHook):
+            def __init__(self):
+                super(clsh_with_after, self).__init__()
+
+            def before(self, klass):
+                return klass
+
+            def after(self, klass):
+                return klass
+
         self.assertRaises(Exception, clsh_with_after)
         
     def test_setup_with_wrong_hook_type(self):
@@ -213,6 +190,26 @@ class TestClass(unittest.TestCase):
         self.assertEqual(C().do_something(), "do something!")
 
     def _test_basic_inhert(self):
+        """
+        Test Usage for inheritance
+        """
+
+        class cls_inhert_parent(object):
+            def __init__(self):
+                pass
+
+            @funhook.attach_([h_test()]) 
+            def func(self, n):
+                return n + 1
+
+        @funhook.setup_([])
+        class cls_inhert_child(cls_inhert_parent):
+            def __init__(self):
+                pass
+
+            def func(self, n):
+                return n + 100
+
         sc = cls_inhert_child()
         # TODO: not implemented yet
         #self.assertEqual(sc.func(1), 102)
